eab555a0e48277557483d49e29c8b494cbeef426
Amirul Momenin <amirrucst@gmail.com>
2020-01-01 21:37:59 +0600

    Update subcat.php

--- subcat.php
+++ subcat.php
@@ -5,7 +5,7 @@
 	switch($cmd){
 		case "load_subcat": 
 		    //load all data
-			$sql = "SELECT * FROM sub_category WHERE id='".$_REQUEST['category_id']."'";
+			$sql = "SELECT * FROM sub_category Where category_id='".$_REQUEST['category_id']."'";
 			$result = $conn->query($sql);
 			if ($result->num_rows > 0) {
 				// output data of each row
